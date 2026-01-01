12cbfbc0f978b75ac3c7710fae7d15cd9b4b571c
Geva Kipper <gevakip@gmail.com>
2026-01-01 15:53:24 +0200

    Change model path to openrouter version

--- updater.py
+++ updater.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import archive_utils
 import utils
 
-MODEL = 'google/gemini-3-pro-preview'
+MODEL = 'openrouter/google/gemini-3-pro-preview'
 
 LIKE_BUTTON_CODE = """<!-- Add this block to embed the like button -->
     <script src="/static/js/like_button.js"></script>
